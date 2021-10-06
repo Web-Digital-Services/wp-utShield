ce6fe4ffb04932411abf69f8bd2f1f5d417321dc
ozunaj <ozunaj@uthscsa.edu>
2021-10-06 10:13:54 -0500

    Removing Args from template file
    FactWP support helped me fix the issue. Needed to remove the args and custom query from the file.

--- wp-content/themes/wp-shield/template-parts/facetwp/physician-loop.php
+++ wp-content/themes/wp-shield/template-parts/facetwp/physician-loop.php
@@ -1,20 +1,6 @@
 <div class="grid-x grid-margin-x">
 	<?php
-		$physician_Last_name = types_render_field("last-name"); 
-
-		$physician_args = 
-			array(
-			'posts_per_page' => 12,
-			'post_type' => 'physicians-profile',
-			'post__not_in'   => array( $post->ID ),
-			'orderby'        => $physician_Last_name,
-			'order'          => 'ASC',
-			'facetwp' => true,
-		);
-		
-		$query_physicians = new WP_Query($physician_args); 
-		
-		if( $query_physicians->have_posts() ) { while( $query_physicians->have_posts() ) { $query_physicians->the_post();?>
+        if ( have_posts() ) { while ( have_posts() ) { the_post();?>
 			<div class="card cell small-12 medium-4 large-4">
 				<?php
 					if ( has_post_thumbnail() ) {
@@ -29,9 +15,8 @@
 					<p>specialty</p>
 				</div>
 			</div>
-		<?php
+			<?php
 			}
-			wp_reset_postdata();
 		}
 	?>
 </div>
