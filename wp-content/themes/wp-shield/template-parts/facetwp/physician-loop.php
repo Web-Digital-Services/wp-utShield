7c99757d3d6d16e48b1135c129b38bcfdc301fe2
ozunaj <ozunaj@uthscsa.edu>
2021-10-05 21:40:56 -0500

    Testing Physician Loop with FacetWP

---
+++ wp-content/themes/wp-shield/template-parts/facetwp/physician-loop.php
@@ -0,0 +1,34 @@
+<div class="grid-x grid-margin-x">
+	<?php
+		$physician_args = 
+			array(
+			'posts_per_page' => 12,
+			'post_type' => 'physicians-profile',
+			'post__not_in'   => array( $post->ID ),
+			'orderby'        => 'title',
+			'order'          => 'ASC'
+		);
+		
+		$query_physicians = new WP_Query($physician_args); 
+		
+		if( $query_physicians->have_posts() ) { while( $query_physicians->have_posts() ) { $query_physicians->the_post();?>
+			<div class="card cell small-12 medium-4 large-4">
+				<?php
+					if ( has_post_thumbnail() ) {
+						the_post_thumbnail('medium');
+					}
+					else {
+						echo '<img alt="This provider does not have a picture" src="https://utphysicians.wpengine.com/wp-content/uploads/2021/09/uthealth-provider-profile-fallback-mobile_0.jpg">';
+					}
+				?>
+				<div class="card-section">
+					<h5><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h5>
+					<p>specialty</p>
+				</div>
+			</div>
+		<?php
+			}
+			wp_reset_postdata();
+		}
+	?>
+</div>
