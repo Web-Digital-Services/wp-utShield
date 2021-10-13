9b14f1cc84f17813cbdf2562a0796b32512a2ffa
ozunaj <ozunaj@uthscsa.edu>
2021-10-13 16:55:55 -0500

    Quick update to Hero Banner Page template

--- wp-content/themes/wp-shield/page-templates/hero-banner-page.php
+++ wp-content/themes/wp-shield/page-templates/hero-banner-page.php
@@ -5,7 +5,7 @@ Template Name: Hero Banner
 load_theme_design('header'); ?>
 <?php get_template_part( 'template-parts/banners/banner-homepage', get_post_format() ); ?>
 <div class="main-container">
-	<div class="main-grid">
+	<div>
 		<main class="page-content">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
