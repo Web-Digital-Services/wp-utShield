9a6edf9082ced763997d24ba563a88973efe26ff
ozunaj <ozunaj@uthscsa.edu>
2021-10-18 10:46:38 -0500

    Basic page tweaks
    - Fixed breadcrumbs
    - Fixed default page width when using WP Bakery editor.

--- wp-content/themes/wp-shield/page-templates/basic-page.php
+++ wp-content/themes/wp-shield/page-templates/basic-page.php
@@ -6,7 +6,7 @@ load_theme_design('header'); ?>
 <div class="main-container">
 	<div class="main-grid">
 		<main class="page-content">
-			<?php if ( function_exists('yoast_breadcrumb') ) { yoast_breadcrumb('<p id="breadcrumbs">','</p>'); } ?>
+			<?php if ( function_exists('yoast_breadcrumb') ) { yoast_breadcrumb('<ul class="breadcrumbs" id="breadcrumbs">','</ul><br><br>'); } ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 				<h1><?php the_title();?></h1>
 				<?php the_content(); ?>
