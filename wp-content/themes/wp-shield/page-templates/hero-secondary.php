a97fe6e49f54683dc88950ba380adbb0504916fa
ozunaj <ozunaj@uthscsa.edu>
2021-10-22 09:21:24 -0500

    Update hero-secondary.php

--- wp-content/themes/wp-shield/page-templates/hero-secondary.php
+++ wp-content/themes/wp-shield/page-templates/hero-secondary.php
@@ -4,7 +4,7 @@ Template Name: Hero (Secondonary)
 */
 load_theme_design('header'); ?>
 <?php get_template_part( 'template-parts/banners/hero-secondary', get_post_format() ); ?>
-<main class="page-content">
+<main class="shell shell--alt grid-container">
 	<?php while ( have_posts() ) : the_post(); ?>
 		<?php the_content(); ?>
 	<?php endwhile; ?>
