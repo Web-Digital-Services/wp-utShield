b93dd01077a8d06586d9d2a80d9f2f0ff8bdd6c5
ozunaj <ozunaj@uthscsa.edu>
2022-03-04 14:25:01 -0600

    Initial Commit for Header Re-work

--- wp-content/themes/wp-shield/header.php
+++ wp-content/themes/wp-shield/header.php
@@ -31,45 +31,23 @@
 		<a href="<?php echo get_option('home'); ?>"><img  alt='Mobile Logo in White' src='<?php ut_display_logo('mobile'); ?>'></a>
 		<button class="menu-icon" type="button" data-toggle="regular">Menu</button>
 		</header>
-	<!-- Level 3 Header -->
-	<div class="title-bar global-bar">
-		<div class="title-bar-left"></div>  
-		<div class="title-bar-right">
-			<?php 
-				$part_of_ut_health_label = get_theme_mod( 'part_of_ut_health_label');
-				$part_of_ut_health_url = get_theme_mod( 'part_of_ut_health_url');
-								
-				if (!empty($part_of_ut_health_url) && ($part_of_ut_health_label)){
-					echo '<p><a aria-label="Mobile Breadcrumb to parent site" href="'. $part_of_ut_health_url .'"><i class="fas fa-star"></i> ' . $part_of_ut_health_label . '</a></p>';
-				}else{
-					echo '<p><a aria-label="Mobile Breadcrumb to parent site" href="https://www.uthscsa.edu/"><i class="fas fa-star"></i> Part of UT Health San Antonio</a></p>';
-				} 
-			?>
-		</div>
-	</div>
-	<?php 
-		$site_title = get_bloginfo ( 'description' );
-		if (!empty($site_title)){
-			$pipe = 'pipe';
-		}else{
-			$pipe = '';
-		}
-	?>
-	<header id="regular">
+
+	<header class="off-canvas-content" id="new" data-off-canvas-content="" style="display: block;">
 		<div class="group">
 			<div class="logo <?php echo $pipe; ?>">
 				<a href="<?php echo get_option('home'); ?>"><img src='<?php ut_display_logo("desktop"); ?>' alt='Desktop Logo'></a>
+				<?php 
+					$site_title = get_bloginfo ( 'description' );
+					if (!empty($site_title)){
+						$pipe = 'pipe';
+					}else{
+						$pipe = '';
+					}
+				?>
 				<p><?php echo $site_title; ?></p>
 			</div>
 			<div class="right">
-				<form method="get" class="search" action="<?php bloginfo('url'); ?>/">
-					<!-- <label for="desktop-search" class="element-invisible">Desktop Search</label>-->
-					<input class="search-box" type="search" size="5" name="s" id="desktop-search" value="Search" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/>
-					<button type="submit">
-						<i class="fa fa-search"></i>
-					</button>
-				</form>
-				<?php 
+					<?php 
 					$UTH_add_give_button_setting = get_theme_mod( 'UTH_add_give_button_setting'); 
 					$UTH_contact_phone_number = get_theme_mod( 'UTH_contact_phone_number'); 
 					$UTH_contact_url = get_theme_mod( 'UTH_contact_url'); 
@@ -94,23 +72,84 @@
 					}
 					echo '</div>';
 				?>
+                <a href="#" data-toggle="offCanvasRight" aria-expanded="false" aria-controls="offCanvasRight">
+                    <span class="fa-stack outline">
+                        <i class="fas fa-circle fa-stack-2x"></i>
+                        <i class="fas fa-search fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <span>Search/Quicklinks</span>
+                </a>
+            </div>
+        </div>
+	</header>
+
+<?php 
+	//Get Banner content from the DB and assign to variables
+	$banner_button_text = get_post_meta( get_the_ID(), 'banner-button-text', true ); 
+	$banner_button_url = get_post_meta( get_the_ID(), 'banner-button-url', true ); 
+	$banner_button_text2 = get_post_meta( get_the_ID(), 'banner-button-text2', true ); 
+	$banner_button_url2 = get_post_meta( get_the_ID(), 'banner-button-url2', true ); 
+	$banner_title = get_post_meta( get_the_ID(), 'banner-title', true);
+	$banner_byline = get_post_meta( get_the_ID(), 'banner-byline', true );
+	$banner_byline = get_post_meta( get_the_ID(), 'banner-byline', true );
+	
+	$grid_layout = get_post_meta( $post->ID, 'banner_grid_layout_key', true );
+	if (empty($grid_layout) || ($grid_layout == '84')){
+		$column_left_css = 'cell large-8 medium-8 small-12 ';
+		$column_right_css = 'cell large-4 medium-4 small-12';
+	}elseif($grid_layout == '75'){
+		$column_left_css = 'cell large-7 medium-7 small-12';
+		$column_right_css = 'cell large-5 medium-5 small-12';
+	}else{
+		$column_left_css = 'cell large-8 medium-8 small-12';
+		$column_right_css = 'cell large-4 medium-4 small-12';
+	}
+?>
+<header>
+	<section class="hero bleed">
+		<div class="grid-container">
+			<div class="grid-x margin-x">
+				<div class="<?php echo $column_left_css; ?>">
+					<?php         
+						if( !empty( $banner_title ) ) {
+							echo '<h1 class="close">' . $banner_title, '</h1>';
+						}else{
+							the_title('<h1 class="close">', '</h1>');
+						}
+						// Checks and displays the retrieved value
+						if( !empty( $banner_byline ) ) {
+							echo '<p class="banner-text">' . $banner_byline, '</p>';
+						}
+						if( !empty( $banner_button_text ) ) {
+							echo '<div class="button-group">';
+							if( !empty( $banner_button_text ) ) { echo '<p><a class="button" href="' . $banner_button_url, '">' . $banner_button_text, '</a></p>'; }
+							if( !empty( $banner_button_text2 ) ) { echo '<p><a class="button ghost" href="' . $banner_button_url2, '">' . $banner_button_text2, '</a></p>'; }
+							echo '</div>';
+						} 
+					?>
+				</div>
+				<div class="<?php echo $column_right_css; ?>">          
+					<?php
+						if (function_exists('display_featured_media')) {
+							display_featured_media('large', 'close');
+						} elseif ( has_post_thumbnail() ){
+							the_post_thumbnail();
+						}else{
+							echo '<p class="white-text">Please install UT Featured Video Plugin</p>';
+						}
+					?>
+				</div>
 			</div>
 		</div>
-		<nav class="site-navigation nav-bar" id="<?php foundationpress_mobile_menu_id(); ?>">
+	</section>
+</header>
+	<header id="">
+
+		<nav class="nav-bar" id="new-full <?php foundationpress_mobile_menu_id(); ?>">
 			<div class="top-bar">	
 				<div class="top-bar-left">
 					<?php foundationpress_main_menu(); ?>
 				</div>
-				<?php if ( has_nav_menu( 'utility-nav' ) ) {
-						echo '<div class="top-bar-right">';
-						if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ){
-							get_template_part( 'template-parts/utility-nav' ); 
-						}
-					echo '</div>';
-					} else {
-						echo '<!-- no utility nav --> ';
-					}
-				?>
 			</div>
 		</nav>
 	</header>
--- wp-content/themes/wp-shield/page.php
+++ wp-content/themes/wp-shield/page.php
@@ -12,66 +12,6 @@
 
 load_theme_design('header'); ?>
 <!-- Start building tha page template banner -->
-<?php 
-	//Get Banner content from the DB and assign to variables
-	$banner_button_text = get_post_meta( get_the_ID(), 'banner-button-text', true ); 
-	$banner_button_url = get_post_meta( get_the_ID(), 'banner-button-url', true ); 
-	$banner_button_text2 = get_post_meta( get_the_ID(), 'banner-button-text2', true ); 
-	$banner_button_url2 = get_post_meta( get_the_ID(), 'banner-button-url2', true ); 
-	$banner_title = get_post_meta( get_the_ID(), 'banner-title', true);
-	$banner_byline = get_post_meta( get_the_ID(), 'banner-byline', true );
-	$banner_byline = get_post_meta( get_the_ID(), 'banner-byline', true );
-	
-	$grid_layout = get_post_meta( $post->ID, 'banner_grid_layout_key', true );
-	if (empty($grid_layout) || ($grid_layout == '84')){
-		$column_left_css = 'cell large-8 medium-8 small-12 ';
-		$column_right_css = 'cell large-4 medium-4 small-12';
-	}elseif($grid_layout == '75'){
-		$column_left_css = 'cell large-7 medium-7 small-12';
-		$column_right_css = 'cell large-5 medium-5 small-12';
-	}else{
-		$column_left_css = 'cell large-8 medium-8 small-12';
-		$column_right_css = 'cell large-4 medium-4 small-12';
-	}
-?>
-<header>
-	<section class="hero bleed">
-		<div class="grid-container">
-			<div class="grid-x margin-x">
-				<div class="<?php echo $column_left_css; ?>">
-					<?php         
-						if( !empty( $banner_title ) ) {
-							echo '<h1 class="close">' . $banner_title, '</h1>';
-						}else{
-							the_title('<h1 class="close">', '</h1>');
-						}
-						// Checks and displays the retrieved value
-						if( !empty( $banner_byline ) ) {
-							echo '<p class="banner-text">' . $banner_byline, '</p>';
-						}
-						if( !empty( $banner_button_text ) ) {
-							echo '<div class="button-group">';
-							if( !empty( $banner_button_text ) ) { echo '<p><a class="button" href="' . $banner_button_url, '">' . $banner_button_text, '</a></p>'; }
-							if( !empty( $banner_button_text2 ) ) { echo '<p><a class="button ghost" href="' . $banner_button_url2, '">' . $banner_button_text2, '</a></p>'; }
-							echo '</div>';
-						} 
-					?>
-				</div>
-				<div class="<?php echo $column_right_css; ?>">          
-					<?php
-						if (function_exists('display_featured_media')) {
-							display_featured_media('large', 'close');
-						} elseif ( has_post_thumbnail() ){
-							the_post_thumbnail();
-						}else{
-							echo '<p class="white-text">Please install UT Featured Video Plugin</p>';
-						}
-					?>
-				</div>
-			</div>
-		</div>
-	</section>
-</header>
 <main class="grid-container">
 	<?php while ( have_posts() ) : the_post(); ?>
 		<?php the_content(); ?>
