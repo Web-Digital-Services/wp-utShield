d86f97f0822e098a5483e8929845165354a13d87
ozunaj <ozunaj@uthscsa.edu>
2022-05-17 16:33:07 -0500

    Fixing header closing tag and image sizes.

--- wp-content/themes/wp-shield/template-parts/content.php
+++ wp-content/themes/wp-shield/template-parts/content.php
@@ -12,25 +12,25 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header>
-	<?php
-		if ( is_single() ) {
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		} else {
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		}
-	?>
-		<?php foundationpress_entry_meta(); ?>
 		<?php
-			if (function_exists('display_featured_media')) {
-				display_featured_media('large', 'far', 'true');
+			if ( is_single() ) {
+				the_title( '<h1 class="entry-title">', '</h1>' );
 			} else {
-				echo '<figure>';
-				the_post_thumbnail('large', array( 'class' => 'featured-image-post' ));
-				echo '<figcaption><?php the_post_thumbnail_caption();?></figcaption>';
-				echo'</figure>';
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 			}
 		?>
 	</header>
+	<?php foundationpress_entry_meta(); ?>
+	<?php
+		if (function_exists('display_featured_media')) {
+			display_featured_media('large', 'far', 'true');
+		} else {
+			echo '<figure>';
+			the_post_thumbnail('large', array( 'class' => 'featured-image-post' ));
+			echo '<figcaption><?php the_post_thumbnail_caption();?></figcaption>';
+			echo'</figure>';
+		}
+	?>
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php edit_post_link( __( '(Edit)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
