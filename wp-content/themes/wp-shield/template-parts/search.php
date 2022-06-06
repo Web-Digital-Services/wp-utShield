f896e996aee3051460bd440d658500866c293539
ozunaj <ozunaj@uthscsa.edu>
2022-06-06 10:56:04 -0500

    Update search.php

--- wp-content/themes/wp-shield/template-parts/search.php
+++ wp-content/themes/wp-shield/template-parts/search.php
@@ -11,18 +11,6 @@
 ?>
 <li>
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header>
-			<?php
-				if ( is_single() ) {
-					the_title( '<h2 class="entry-title">', '</h2>' );
-				} else {
-					the_title( '<h2 class="h4"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-				}
-			?>
-		</header>
-		<div class="entry-content">
-			<?php the_excerpt(); ?>
-			<?php edit_post_link( __( '(Edit this entry)', 'foundationpress' ), '<span class="edit-link">', '</span>' ); ?>
-		</div>
+		<?php get_template_part( 'template-parts/snippet', 'news' ); ?>
 	</article>
 </li>
