e2d8ddacffff91c585051ff1b00c58193f833b7e
Jacqueline Simons <simonsj@uthscsa.edu>
2023-07-12 15:22:57 -0500

    Wrap time in paragraph tag

--- wp-content/themes/wp-shield/library/modified-core/entry-meta.php
+++ wp-content/themes/wp-shield/library/modified-core/entry-meta.php
@@ -10,6 +10,6 @@ if ( ! function_exists( 'foundationpress_entry_meta' ) ) :
 	function foundationpress_entry_meta() {
 		/* translators: %1$s: current date, %2$s: current time */
 		echo '<p class="byline author close">' . __( 'Shared By:', 'foundationpress' ) . ' <a href="' . get_author_posts_url( get_the_author_meta( 'ID' ) ) . '" rel="author" class="fn">' . get_the_author() . '</a></p>';
-		echo '<time class="updated" datetime="' . get_the_time( 'c' ) . '">' . sprintf( __( '%1$s', 'foundationpress' ), get_the_date(), get_the_time() ) . '</time>';
+		echo '<p><time class="updated" datetime="' . get_the_time( 'c' ) . '">' . sprintf( __( '%1$s', 'foundationpress' ), get_the_date(), get_the_time() ) . '</time></p>';
 	}
 endif;
