61a59c5b56d0a1bdffdef0d426b49f4ea7a3aa74
Jacqueline Simons <simonsj@uthscsa.edu>
2024-10-08 11:57:13 -0500

    Fix error if user selects Mission from theme options

--- wp-content/themes/wp-shield/library/wp-shield/customizer/register-menus.php
+++ wp-content/themes/wp-shield/library/wp-shield/customizer/register-menus.php
@@ -99,3 +99,33 @@ if ( ! function_exists( 'uth_fifth_quick_links' ) ) {
         ));
     }
 }
+/** Display all sidebar items in a quicklinks panel pop out */
+function UTH_render_quick_links_menu(){
+    if ( has_nav_menu( 'uth_primary_quick_links' ) ) {
+        $primary_menu_name = 'uth_primary_quick_links';
+        $locations = get_nav_menu_locations();
+        $primary_menu_id = $locations[ $primary_menu_name ] ;
+        $primary_quicklinks_menu_object = wp_get_nav_menu_object($primary_menu_id);
+
+        echo '<div class="off-canvas position-right bullet-list UTH_Off_canvas" id="offCanvas" role="navigation" aria-labelledby="quickLinks" data-off-canvas>';
+        echo '<button id="quickLinks" class="close-button" aria-label="Close menu" type="button" data-close=""><span aria-hidden="true">×</span></button>';
+        echo '<h2 class="close">' . $primary_quicklinks_menu_object->name . '</h2>';
+        uth_primary_quick_links();
+
+        if ( has_nav_menu( 'uth_secondary_quick_links' ) ) {
+            $secondary_menu_name = 'uth_secondary_quick_links';
+            $secondary_menu_id = $locations[ $secondary_menu_name ] ;
+            $secondary_quicklinks_menu_object = wp_get_nav_menu_object($secondary_menu_id);
+            echo '<h2 class="close">' . $secondary_quicklinks_menu_object->name . '</h2>';
+            uth_secondary_quick_links();
+        }
+        if ( has_nav_menu( 'uth_third_quick_links' ) ) {
+            $third_menu_name = 'uth_third_quick_links';
+            $third_menu_id = $locations[ $third_menu_name ] ;
+            $third_quicklinks_menu_object = wp_get_nav_menu_object($third_menu_id);
+            echo '<h2 class="close">' . $third_quicklinks_menu_object->name . '</h2>';
+            uth_third_quick_links();
+        }
+        echo '</div>';
+    } 
+}
